cc2888cd8fec1270b2c353671262937ac68ad9b8
Atom <LucasSariol@gmail.com>
2025-11-21 12:35:42 -0500

    Changed the Time display and function name for logs to console.

--- internal/adapters/twitch/bot/io.go
+++ internal/adapters/twitch/bot/io.go
@@ -105,5 +105,5 @@ func (c *TwitchClient) printE(e eventsub.EventSubMessage) {
 
 func timestamp() string {
 
-	return time.Now().Format("2006-01-02 15-04-05.000")
+	return time.Now().Format("2006-01-02 15:04:05.00")
 }
